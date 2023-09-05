1e27490028c42281b10f1722956f017af035502d
Thái Hoàng <mr.hoangminhthai@gmail.com>
2023-09-05 11:45:44 +0700

    update build script (#15)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,15 +2,21 @@ import { PrismaClient } from '@prisma/client'
 
 import { NODE_ENV } from '../config/env'
 
-let prisma: PrismaClient
+console.log('NODE_ENV:', NODE_ENV)
 
-if (NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+// let prisma: PrismaClient
+//
+// if (NODE_ENV === 'production') {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient()
+//   }
+//   prisma = global.prisma
+// }
+//
+// export { prisma }
+
+const prisma = new PrismaClient()
 
 export { prisma }
